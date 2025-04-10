f3152b1d0bebca1f2a53ca0b760de6ed3c538b63
Daniel <jbcalc.28.9@gmail.com>
2025-04-10 13:33:04 -0500

    feat:Solving Temperature Summary Challenge

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,11 @@ interface TemperatureSummary {
   average: number
 }
 
+const readingsFiltered: TemperatureReading[] = []
+
 export function processReadings(readings: TemperatureReading[]): void {
   // add here your code
+  readingsFiltered.push(...readings)
 }
 
 export function getTemperatureSummary(
@@ -22,5 +25,32 @@ export function getTemperatureSummary(
   city: string,
 ): TemperatureSummary | null {
   //add here your code
-  return null
+
+  const listFiltered = readingsFiltered
+    .filter((item) => {
+      return (
+        item.city === city && item.time.toDateString() === date.toDateString()
+      )
+    })
+    .map((item) => item.temperature)
+
+  const firstTemp = listFiltered[0]
+  const lastTemp = listFiltered[listFiltered.length - 1]
+  const highTemp = listFiltered.sort((tempA, tempB) => {
+    return tempB - tempA
+  })[0]
+  const lowTemp = listFiltered.sort((tempA, tempB) => {
+    return tempA - tempB
+  })[0]
+  const average =
+    listFiltered.reduce((acc, item) => acc + item, 0) / listFiltered.length
+
+  const summary: TemperatureSummary = {
+    first: firstTemp,
+    last: lastTemp,
+    high: highTemp,
+    low: lowTemp,
+    average: average,
+  }
+  return listFiltered.length !== 0 ? summary : null
 }
